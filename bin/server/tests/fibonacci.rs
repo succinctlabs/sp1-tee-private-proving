c0928f61a422642d01f1b2912104d7ac6675de57
leruaa <aurelien.catinon@gmail.com>
2026-02-04 13:44:22 -0800

    test: use env vars

--- bin/server/tests/fibonacci.rs
+++ bin/server/tests/fibonacci.rs
@@ -4,18 +4,20 @@ const FIBONACCI_ELF: &[u8] = include_bytes!("../fixtures/fibonacci.elf");
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_prove() {
+    dotenv::dotenv().ok();
     sp1_sdk::utils::setup_logger();
 
-    let client = ProverClient::builder()
-        .network()
-        .private()
-        .private_key("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d") // 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
-        .build();
+    let client = ProverClient::builder().network().private().build();
 
     let (pk, vk) = client.setup(FIBONACCI_ELF);
     let mut stdin = SP1Stdin::new();
 
     stdin.write(&10);
+
+    let (_, report) = client.execute(FIBONACCI_ELF, &stdin).run().unwrap();
+
+    println!("Cycles: {}", report.total_instruction_count());
+    println!("Gas used: {}", report.gas.unwrap());
 
     let proof = client
         .prove(&pk, &stdin)
